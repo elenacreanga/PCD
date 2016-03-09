be9055cd96bb247e20c4a257ff37ed11f20f9721
elenacreanga <elena.creanga.mail@gmail.com>
2016-03-09 05:58:18 +0200

    add angular and create tripsController

---
+++ H1/TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,10 @@
+﻿(function() {
+    "use strict";
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+        var vm = this;
+        vm.name = "Elena";
+    }
+})();
