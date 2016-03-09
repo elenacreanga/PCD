2165c92b77973505900c2c8f40bf41867426d0e5
elenacreanga <elena.creanga.mail@gmail.com>
2016-03-09 06:25:39 +0200

    add get trips call from api

--- H1/TheWorld/src/TheWorld/wwwroot/js/tripsController.js
+++ H1/TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -3,8 +3,38 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         var vm = this;
-        vm.name = "Elena";
+        vm.trips = [
+            {
+                name: "US Trip",
+                created: new Date()
+            },
+            {
+                name: "World Trip",
+                created: new Date()
+            }
+        ];
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function(response) {
+                angular.copy(response.data, vm.trips);
+            }, function(error) {
+                vm.errorMessage = "Failed to load data:" + error;
+            })
+            .finally(function() {
+                vm.isBusy = false;
+            });
+
+        vm.newTrip = {};
+        vm.addTrip = function() {
+            vm.trips.push({ name: vm.newTrip.name, created: new Date() });
+            vm.newTrip = {};
+        };
+
+
     }
 })();
