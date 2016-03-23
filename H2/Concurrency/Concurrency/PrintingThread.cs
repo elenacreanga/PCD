b8982256319299d318b9df0014e56a00364f867a
elenacreanga <elena.creanga.mail@gmail.com>
2016-03-23 09:02:53 +0200

    Use Syncronization Attribute

--- H2/Concurrency/Concurrency/PrintingThread.cs
+++ H2/Concurrency/Concurrency/PrintingThread.cs
@@ -1,32 +1,34 @@
 ﻿using System;
+using System.Runtime.Remoting.Contexts;
 using System.Threading;
 
 namespace Concurrency
 {
-    public class PrintingThread
+    [Synchronization]
+    public class PrintingThread : ContextBoundObject
     {
         private readonly object threadLock = new object();
 
         public void ThreadJob()
         {
-            Monitor.Enter(threadLock);
-            try
+            //Monitor.Enter(threadLock);
+            //try
             //lock (threadLock)
+            //{
+            Console.WriteLine(Thread.CurrentThread.Name);
+            Console.WriteLine($"Here are the numbers for: {Thread.CurrentThread.Name}");
+            for (var i = 0; i < 10; i++)
             {
-                Console.WriteLine(Thread.CurrentThread.Name);
-                Console.WriteLine($"Here are the numbers for: {Thread.CurrentThread.Name}");
-                for (var i = 0; i < 10; i++)
-                {
-                    var random = new Random();
-                    Thread.Sleep(1000 * random.Next(2));
-                    Console.Write($"{i} ");
-                }
-                Console.WriteLine();
+                var random = new Random();
+                Thread.Sleep(1000 * random.Next(2));
+                Console.Write($"{i} ");
             }
-            finally
-            {
-                Monitor.Exit(threadLock);
-            }
+            Console.WriteLine();
+            //}
+            //finally
+            //{
+            //    Monitor.Exit(threadLock);
+            //}
         }
     }
 }
